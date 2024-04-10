62a1b1a534a12019fb903adb72be5284c0590762
rickherrera3001 <rihr.3001@gmail.com>
2024-04-10 15:13:23 -0600

    Initial Commit

---
+++ Duck Duke Go Search/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Duck Duke Go Search
+//
+//  Created by Ricardo Developer on 10/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Duck Duke Go Search/Duck_Duke_Go_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Duck_Duke_Go_SearchApp.swift
+//  Duck Duke Go Search
+//
+//  Created by Ricardo Developer on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Duck_Duke_Go_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
